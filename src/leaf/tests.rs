eb3ff8397e8907f9d2f36fc0c3f32ebec070e637
Felix Karg <f.karg10@gmail.com>
2022-07-17 02:17:00 +0200

    begun writing/adding tests, fixed uncovered bugs

--- src/leaf/tests.rs
+++ src/leaf/tests.rs
@@ -1,6 +1,146 @@
 use super::*;
 
 #[test]
-fn exploration() {
-    assert_eq!(2 + 2, 4);
+fn creation() {
+    let l = Leaf::new(0);
+    assert_eq!(
+        l,
+        Leaf {
+            parent: 0,
+            value: 0,
+            nums: 0
+        }
+    );
+}
+
+#[test]
+fn push_0() {
+    let mut l = Leaf::new(0);
+    l.push(false).ok();
+    l.push(false).ok();
+    l.push(false).ok();
+    l.push(false).ok();
+    assert_eq!(
+        l,
+        Leaf {
+            parent: 0,
+            value: 0,
+            nums: 4
+        }
+    );
+}
+
+#[test]
+fn push_1() {
+    let mut l = Leaf::new(0);
+    l.push(true).ok();
+    l.push(true).ok();
+    l.push(true).ok();
+    l.push(true).ok();
+    assert_eq!(
+        l,
+        Leaf {
+            parent: 0,
+            value: 15,
+            nums: 4
+        }
+    );
+}
+
+#[test]
+fn push_all_0() {
+    let mut l = Leaf::new(0);
+    for _ in 0..LeafValue::BITS {
+        l.push(false).ok();
+    }
+    assert_eq!(
+        l,
+        Leaf {
+            parent: 0,
+            value: 0,
+            nums: LeafValue::BITS as u8
+        }
+    );
+}
+
+#[test]
+fn push_all_1() {
+    let mut l = Leaf::new(0);
+    for _ in 0..LeafValue::BITS {
+        l.push(true).ok();
+    }
+    assert_eq!(
+        l,
+        Leaf {
+            parent: 0,
+            value: LeafValue::MAX,
+            nums: LeafValue::BITS as u8
+        }
+    );
+}
+
+#[test]
+fn insert_0() {
+    let mut l = Leaf::new(0);
+    l.insert(0, false).ok();
+    l.insert(0, false).ok();
+    l.insert(0, false).ok();
+    l.insert(0, false).ok();
+    assert_eq!(
+        l,
+        Leaf {
+            parent: 0,
+            value: 0,
+            nums: 4
+        }
+    );
+}
+
+#[test]
+fn insert_1() {
+    let mut l = Leaf::new(0);
+    l.insert(0, true).ok();
+    l.insert(0, true).ok();
+    l.insert(0, true).ok();
+    l.insert(0, true).ok();
+    assert_eq!(
+        l,
+        Leaf {
+            parent: 0,
+            value: 15,
+            nums: 4
+        }
+    );
+}
+
+#[test]
+fn insert_all_0() {
+    let mut l = Leaf::new(0);
+    for _ in 0..LeafValue::BITS {
+        l.insert(0, false).ok();
+    }
+    assert_eq!(
+        l,
+        Leaf {
+            parent: 0,
+            value: 0,
+            nums: LeafValue::BITS as u8
+        }
+    );
+}
+
+#[test]
+fn insert_all_1() {
+    let mut l = Leaf::new(0);
+    for _ in 0..LeafValue::BITS {
+        l.insert(0, true).ok();
+    }
+    assert_eq!(
+        l,
+        Leaf {
+            parent: 0,
+            value: LeafValue::MAX,
+            nums: LeafValue::BITS as u8
+        }
+    );
 }
--- src/primitive_static.rs
+++ src/primitive_static.rs
@@ -1,45 +1,173 @@
 use super::traits::StaticBitVec;
+use core::arch::x86_64::{_pdep_u64, _popcnt64, _tzcnt_u64};
 
-trait UnsafeSelect {
+trait UnsafeBitVec {
     unsafe fn select_internal(&self, bit: bool, n: usize) -> usize;
+
+    unsafe fn rank_internal(&self, bit: bool, index: usize) -> usize;
 }
 
-impl UnsafeSelect for u64 {
-    #[cfg(not(all(target_feature = "bmi2", target_feature = "bmi1")))]
+impl UnsafeBitVec for u64 {
+    #[cfg(not(all(
+        target_arch = "x86_64",
+        target_feature = "bmi1",
+        target_feature = "bmi2"
+    )))]
     unsafe fn select_internal(&self, bit: bool, n: usize) -> usize {
-        let mut pos = 0;
-        let mut i = n;
-        for shift in (0..u64::BITS).rev() {
+        let mut cnt = n;
+        // go over u64 from right to left
+        for shift in 0..u64::BITS {
             if (((self >> shift) & 1) != 0) == bit {
-                if i == 0 {
-                    return pos;
+                // we're looking for `n`-th match, so check for zero first
+                // (insdead of: decrease first)
+                if cnt == 0 {
+                    return shift as usize;
                 }
-                i -= 1;
-                pos += 1;
+                cnt -= 1;
             }
         }
-        panic!("`n`-th `bit`-value not found in {}", self)
+        panic!("`{n}`-th `bit`-value '{bit}' not found in {self:b}")
     }
 
-    #[cfg(all(target_feature = "bmi2", target_feature = "bmi1"))]
+    /// ```text
+    /// Algorithm for determining the position of the jth 1 in a machine word.
+    /// ---
+    /// 1: function PTSELECT(x, j)
+    /// 2:     i ← SHIFTLEFT(1, j)
+    /// 3:     p ← PDEP(i, x)
+    /// 4:     return TZCNT(p)
+    /// ```
+    ///
+    /// taken from <https://arxiv.org/pdf/1706.00990.pdf>.
+    ///
+    /// # Safety
+    /// Only available for `x86_64`-based architecuters supporting feature sets `bmi1` and `bmi2`,
+    /// which were both introduced by the fourth-generation intel
+    /// [haswell](https://en.wikipedia.org/wiki/Haswell_(microarchitecture)) architecture nine
+    /// years ago.
+    ///
+    /// Execute with `RUSTFLAGS="-C target-cpu=native -O" cargo run --release` to get all performance
+    /// benefits and enable proper cpu feature recognition.
+    #[inline]
+    #[cfg(all(
+        target_arch = "x86_64",
+        target_feature = "bmi1",
+        target_feature = "bmi2"
+    ))]
     unsafe fn select_internal(&self, bit: bool, n: usize) -> usize {
-        let array = if bit { *self } else { !self };
-        _tzcnt_u64(_pdep_u64(1 << n, array)) as usize
+        _tzcnt_u64(_pdep_u64(1 << n, if bit { *self } else { !self })) as usize
+    }
+
+    /// Assume `index` to be in the range of `0..63`
+    ///
+    /// popcnt is part of the x86_64 architecture, and faster (probably) than `count_ones`.
+    #[cfg(target_arch = "x86_64")]
+    unsafe fn rank_internal(&self, bit: bool, index: usize) -> usize {
+        if bit {
+            _popcnt64(self.overflowing_shl(u64::BITS - index as u32).0 as i64) as usize
+        } else {
+            _popcnt64((!self).overflowing_shl(u64::BITS - index as u32).0 as i64) as usize
+        }
+    }
+
+    /// Assume `index` to be in the range of `0..63`
+    #[cfg(not(target_arch = "x86_64"))]
+    unsafe fn rank_internal(&self, bit: bool, index: usize) -> usize {
+        if bit {
+            self.overflowing_shl(u64::BITS - index as u32)
+                .0
+                .count_ones() as usize
+        } else {
+            (!self)
+                .overflowing_shl(u64::BITS - index as u32)
+                .0
+                .count_ones() as usize
+        }
     }
 }
 
+/// Container is [`u64`] Bit Vector, indexed from right to left (big endian).
 impl StaticBitVec for u64 {
     type Intern = u64;
 
+    /// TODO: use _popcnt64 für `x86_64` (just for i64?)
+    #[inline]
     fn ones(&self) -> usize {
-        self.count_ones() as usize
+        self.rank(true, Self::BITS as usize)
     }
 
+    /// Right shift self-bits by `index` and `1`-mask for comparison
+    #[inline]
     fn access(&self, index: usize) -> bool {
         self >> index & 1 == 1
     }
 
+    #[inline]
     fn rank(&self, bit: bool, index: usize) -> usize {
+        if index == 0 {
+            return 0;
+        }
+        unsafe { self.rank_internal(bit, index) }
+    }
+
+    #[inline]
+    fn select(&self, bit: bool, n: usize) -> usize {
+        unsafe { self.select_internal(bit, n) }
+    }
+
+    #[inline]
+    fn values(&self) -> Self::Intern {
+        *self
+    }
+}
+
+impl UnsafeBitVec for u128 {
+    #[cfg(not(all(
+        target_arch = "x86_64",
+        target_feature = "bmi1",
+        target_feature = "bmi2"
+    )))]
+    unsafe fn select_internal(&self, bit: bool, n: usize) -> usize {
+        let mut cnt = n;
+        // go over u128 from right to left
+        for shift in 0..u128::BITS {
+            if (((self >> shift) & 1) != 0) == bit {
+                // we're looking for `n`-th match, so check for zero first
+                // (insdead of: decrease first)
+                if cnt == 0 {
+                    return shift as usize;
+                }
+                cnt -= 1;
+            }
+        }
+        panic!("`{n}`-th `bit`-value '{bit}' not found in {self:b}")
+    }
+
+    #[inline]
+    #[cfg(all(
+        target_arch = "x86_64",
+        target_feature = "bmi1",
+        target_feature = "bmi2"
+    ))]
+    unsafe fn select_internal(&self, bit: bool, n: usize) -> usize {
+        let array = if bit { *self } else { !self };
+        // self.value is u128, but pdep and tzcnt only exist for u64
+        // cast to u64 is expected to be lossy.
+        // First, check if `n` is in right or left half of u128
+        let rank = (self as u64).rank_internal(bit, n);
+        if rank >= n {
+            _tzcnt_u64(_pdep_u64(1 << n, array as u64)) as usize
+        } else {
+            64 + _tzcnt_u64(_pdep_u64(
+                1 << (n - rank),
+                array.overflowing_shr(64).0 as u64,
+            )) as usize
+        }
+    }
+
+    #[inline]
+    #[cfg(not(target_arch = "x86_64"))]
+    unsafe fn rank_internal(&self, bit: bool, index: usize) -> usize {
         if bit {
             (self >> index as u32).count_ones() as usize
         } else {
@@ -47,71 +175,192 @@ impl StaticBitVec for u64 {
         }
     }
 
-    fn values(&self) -> Self::Intern {
-        *self
-    }
-
-    fn select(&self, bit: bool, n: usize) -> usize {
-        unsafe { self.select_internal(bit, n) }
-    }
-}
-
-impl UnsafeSelect for u128 {
-    #[cfg(not(all(target_feature = "bmi2", target_feature = "bmi1")))]
-    unsafe fn select_internal(&self, bit: bool, n: usize) -> usize {
-        let mut pos = 0;
-        let mut i = n;
-        for shift in (0..u128::BITS).rev() {
-            if (((self >> shift) & 1) != 0) == bit {
-                if i == 0 {
-                    return pos;
-                }
-                i -= 1;
-                pos += 1;
-            }
-        }
-        panic!("`n`-th `bit`-value not found in {}", self)
-    }
-
-    #[cfg(all(target_feature = "bmi2", target_feature = "bmi1"))]
-    unsafe fn select_internal(&self, bit: bool, n: usize) -> usize {
+    #[inline]
+    #[cfg(target_arch = "x86_64")]
+    unsafe fn rank_internal(&self, bit: bool, index: usize) -> usize {
         let array = if bit { *self } else { !self };
-        // self.value is u64
-        // _tzcnt_u64(_pdep_u64(1 << n, array as u64)) as usize
 
-        // self.value is u128
-        if n < 64 {
-            _tzcnt_u64(_pdep_u64(1 << n, array as u64)) as usize
+        if index < 64 {
+            // only move by u64::BITS instead of u128::BITS to cap left side away in cast to i64
+            _popcnt64(array.overflowing_shl(u64::BITS - index as u32).0 as i64) as usize
         } else {
-            _tzcnt_u64(_pdep_u64(1 << n, (array >> 64) as u64)) as usize + 64
+            _popcnt64(array.overflowing_shl(u128::BITS - index as u32).0 as i64) as usize
         }
     }
 }
 
+/// Container is [`u128`] Bit Vector, indexed from right to left (big endian).
 impl StaticBitVec for u128 {
     type Intern = u128;
 
+    #[inline]
     fn ones(&self) -> usize {
-        self.count_ones() as usize
+        self.rank(true, Self::BITS as usize)
     }
 
+    #[inline]
     fn access(&self, index: usize) -> bool {
         self >> index & 1 == 1
     }
 
+    #[inline]
     fn rank(&self, bit: bool, index: usize) -> usize {
-        if bit {
-            (self >> index as u32).count_ones() as usize
-        } else {
-            ((!self) >> index as u32).count_ones() as usize
+        if index == 0 {
+            return 0;
         }
+        unsafe { self.rank_internal(bit, index) }
     }
 
+    #[inline]
     fn values(&self) -> Self::Intern {
         *self
     }
 
+    #[inline]
     fn select(&self, bit: bool, n: usize) -> usize {
         unsafe { self.select_internal(bit, n) }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::{assert_eq, assert_ne};
+    use quickcheck::TestResult;
+    use quickcheck_macros::quickcheck;
+    use test_case::test_case;
+
+    #[test]
+    fn ones_u64_1() {
+        assert_eq!(4u64.ones(), 1);
+    }
+
+    #[test]
+    fn ones_u64_2() {
+        assert_eq!(5u64.ones(), 2);
+    }
+
+    #[test]
+    fn ones_u64_3() {
+        assert_eq!(7u64.ones(), 3);
+    }
+
+    /// Test if generated number with specific number of ones really has them
+    #[quickcheck]
+    fn ones_u64(n: u64) -> TestResult {
+        if n == 0 {
+            TestResult::from_bool(0u64.ones() == n as usize)
+        } else if n > u64::BITS as u64 {
+            TestResult::discard()
+        } else {
+            TestResult::from_bool((2 ^ (n - 1)).ones() == n as usize)
+        }
+    }
+
+    /// For u64::MAX, all accessed values should be 1
+    #[quickcheck]
+    fn access_u64_1(n: usize) -> TestResult {
+        if n > u64::BITS as usize {
+            TestResult::discard()
+        } else {
+            TestResult::from_bool(u64::MAX.access(n))
+        }
+    }
+
+    /// For 0u64, all accessed values should be 0
+    #[quickcheck]
+    fn access_u64_0(n: usize) -> TestResult {
+        if n > u64::BITS as usize {
+            TestResult::discard()
+        } else {
+            TestResult::from_bool(!0u64.access(n))
+        }
+    }
+
+    #[quickcheck]
+    fn rank_u64(n: usize) -> TestResult {
+        if n >= u64::BITS as usize {
+            return TestResult::discard();
+        } else {
+            // assert_eq!(u64::MAX.rank(true, 0), 0);
+            // assert_eq!(u64::MAX.rank(true, 1), 1);
+            assert_eq!(u64::MAX.rank(true, n), n);
+            assert_eq!(u64::MAX.rank(false, n), 0);
+            assert_eq!(0u64.rank(false, n), n);
+            assert_eq!(0u64.rank(true, n), 0);
+            TestResult::passed()
+        }
+    }
+
+    /// Simple intuitive tests for select on u64
+    #[test]
+    fn select_u64_simpel() {
+        assert_eq!(1u64.select(true, 0), 0);
+        assert_eq!(1u64.select(false, 0), 1);
+        assert_eq!(2u64.select(true, 0), 1);
+        assert_eq!(2u64.select(false, 0), 0);
+        assert_eq!(3u64.select(true, 1), 1);
+        assert_eq!(3u64.select(false, 1), 3);
+        assert_eq!(u64::MAX.select(true, 63), 63);
+    }
+
+
+    /// Test if generated number with specific number of ones really has them
+    #[quickcheck]
+    fn ones_u128(n: u128) -> TestResult {
+        if n == 0 {
+            TestResult::from_bool(0u128.ones() == n as usize)
+        } else if n > u128::BITS as u128 {
+            TestResult::discard()
+        } else {
+            TestResult::from_bool((2 ^ (n - 1)).ones() == n as usize)
+        }
+    }
+
+    /// For u128::MAX, all accessed values should be 1
+    #[quickcheck]
+    fn access_u128_1(n: usize) -> TestResult {
+        if n > u128::BITS as usize {
+            TestResult::discard()
+        } else {
+            TestResult::from_bool(u128::MAX.access(n))
+        }
+    }
+
+    /// For 0u128, all accessed values should be 0
+    #[quickcheck]
+    fn access_u128_0(n: usize) -> TestResult {
+        if n > u128::BITS as usize {
+            TestResult::discard()
+        } else {
+            TestResult::from_bool(!0u128.access(n))
+        }
+    }
+
+    #[quickcheck]
+    fn rank_u128(n: usize) -> TestResult {
+        if n >= u128::BITS as usize {
+            return TestResult::discard();
+        } else {
+            // assert_eq!(u64::MAX.rank(true, 0), 0);
+            // assert_eq!(u64::MAX.rank(true, 1), 1);
+            assert_eq!(u128::MAX.rank(true, n), n);
+            assert_eq!(u128::MAX.rank(false, n), 0);
+            assert_eq!(0u128.rank(false, n), n);
+            assert_eq!(0u128.rank(true, n), 0);
+            TestResult::passed()
+        }
+    }
+
+    /// Simple intuitive tests for select on u128
+    #[test]
+    fn select_u128_simpel() {
+        assert_eq!(1u128.select(true, 0), 0);
+        assert_eq!(1u128.select(false, 0), 1);
+        assert_eq!(2u128.select(true, 0), 1);
+        assert_eq!(2u128.select(false, 0), 0);
+        assert_eq!(3u128.select(true, 1), 1);
+        assert_eq!(3u128.select(false, 1), 3);
+        assert_eq!(u128::MAX.select(true, 63), 63);
     }
 }
